495c2165a77a068d8a9d9cc1b472fb6c38f435d4
benpariswork <benjaminparis@protonmail.com>
2025-03-14 07:12:16 -0700

    chall 2 v1

--- c2.py
+++ c2.py
@@ -1,23 +1,29 @@
 # imports
 import requests
+import socket
+import re
 from colorama import Fore, Style
+def check_headers(x):
+    # get url from user
+    url = x
 
-# get url from user
-url = input("Enter URL: ")
+    host = re.sub(r"^https?:\/\/", "", url)
+    host = re.sub(r"\/?$", "", host)
+    print(f'IP of {host} is {socket.gethostbyname("www.redsiege.com")}')
 
-# Making a get request
-response = requests.get(url)
+    # Making a get request
+    response = requests.get(url)
 
-# Check header exists in response
-if 'Strict-Transport-Security' in response.headers:
-    print(Fore.GREEN + f"Strict-Transport-Security header was found for {url}")
-    print(Style.RESET_ALL)
-else: 
-    print(Fore.RED + f"Strict-Transport-Security header was not found for {url}")
-    print(Style.RESET_ALL)
+    # Check header exists in response
+    if 'Strict-Transport-Security' in response.headers:
+        print(Fore.GREEN + f"Strict-Transport-Security header was found for {host}")
+        print(Style.RESET_ALL)
+    else: 
+        print(Fore.RED + f"Strict-Transport-Security header was not found for {host}")
+        print(Style.RESET_ALL)
 
 with open('links.txt', 'r') as file:
     #Reade each line in file
     for line in file:
         #Run get_data fucntion on IP
-        print_data(line.strip())
+        check_headers(line.strip())
---
+++ tmp.py
@@ -0,0 +1,3 @@
+import socket
+host = "www.twitter.com"
+print(f'IP of {host} is {socket.gethostbyname(host)}')
