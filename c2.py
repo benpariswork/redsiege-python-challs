742d0a78f02581c6a4da0d57e737f9a9999cb5ae
benpariswork <benjaminparis@protonmail.com>
2025-03-14 06:51:18 -0800

    chall 2 clean code

--- c2.py
+++ c2.py
@@ -3,52 +3,51 @@ import requests
 import socket
 import re
 from colorama import Fore, Style
+
+def extract_host(url):
+    # Extract host from URL
+    host = re.sub(r"^https?:\/\/", "", url)
+    host = re.sub(r"\/?$", "", host)
+    return host
+
+def get_ip_address(host):
+    # Get IP address for host
+    return socket.gethostbyname("www.redsiege.com")
+
+def print_header_status(header_name, headers, host, show_value=False):
+    # Check if header exists and print status
+    if header_name in headers:
+        print(Fore.GREEN + f"{header_name} header was found for {host}")
+        if show_value:
+            print(f"{header_name} Header Value:" + headers[header_name])
+        print(Style.RESET_ALL)
+    else: 
+        print(Fore.RED + f"{header_name} header was not found for {host}")
+        print(Style.RESET_ALL)
+
 def check_headers(x):
     # get url from user
     url = x
 
-    host = re.sub(r"^https?:\/\/", "", url)
-    host = re.sub(r"\/?$", "", host)
-    print(f'IP of {host} is {socket.gethostbyname("www.redsiege.com")}')
+    host = extract_host(url)
+    print(f'IP of {host} is {get_ip_address(host)}')
 
     # Making a get request
     response = requests.get(url)
 
-    # Check 'Strict-Transport-Security' header exists in response
-    if 'Strict-Transport-Security' in response.headers:
-        print(Fore.GREEN + f"Strict-Transport-Security header was found for {host}")
-        print(Style.RESET_ALL)
-    else: 
-        print(Fore.RED + f"Strict-Transport-Security header was not found for {host}")
-        print(Style.RESET_ALL)
+    # Check security headers
+    print_header_status('Strict-Transport-Security', response.headers, host)
+    print_header_status('Content-Security-Policy', response.headers, host)
+    print_header_status('X-Frame-Options', response.headers, host)
+    print_header_status('Server', response.headers, host, show_value=True)
 
-    # Check 'Content-Security-Policy' header exists in response
-    if 'Content-Security-Policy' in response.headers:
-        print(Fore.GREEN + f"Content-Security-Policy header was found for {host}")
-        print(Style.RESET_ALL)
-    else: 
-        print(Fore.RED + f"Content-Security-Policy header was not found for {host}")
-        print(Style.RESET_ALL)
+def process_url_file(filename):
+    with open(filename, 'r') as file:
+        #Read each line in file
+        for line in file:
+            #Run get_data function on IP
+            check_headers(line.strip())
 
-    # Check 'X-Frame-Options' header exists in response
-    if 'X-Frame-Options' in response.headers:
-        print(Fore.GREEN + f"X-Frame-Options header was found for {host}")
-        print(Style.RESET_ALL)
-    else: 
-        print(Fore.RED + f"X-Frame-Options header was not found for {host}")
-        print(Style.RESET_ALL)      
-
-    # Check 'Server' header exists in response, return value
-    if 'Server' in response.headers:
-        print(Fore.GREEN + f"Server was found for {host}")
-        print("Server Header Value:" + response.headers['Server'])
-        print(Style.RESET_ALL)
-    else: 
-        print(Fore.RED + f"Server was not found for {host}")
-        print(Style.RESET_ALL)          
-
-with open('links.txt', 'r') as file:
-    #Read each line in file
-    for line in file:
-        #Run get_data function on IP
-        check_headers(line.strip())
+# Main execution
+if __name__ == "__main__":
+    process_url_file('links.txt')
